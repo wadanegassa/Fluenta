9226d2ca7ad9d60980aeec4054ad11b6fc060c0e
wada negassa <promisehispeace@gmail.com>
2026-05-16 15:17:43 +0300

    chore(windows): update generated plugins cmake

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,9 +3,18 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  app_links
+  connectivity_plus
+  flutter_inappwebview_windows
+  flutter_tts
+  permission_handler_windows
+  record_windows
+  speech_to_text_windows
+  url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  jni
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
