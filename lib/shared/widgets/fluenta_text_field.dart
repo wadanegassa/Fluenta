51c95d761c1cf86097518d5922fc02d916ea4d4d
wada negassa <promisehispeace@gmail.com>
2026-05-16 15:23:47 +0300

    feat(shared): add custom fluenta text field widget

---
+++ lib/shared/widgets/fluenta_text_field.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import '../../core/theme/app_colors.dart';
+import '../../core/theme/app_dimensions.dart';
+import '../../core/theme/app_text_styles.dart';
+
+class FluentaTextField extends StatelessWidget {
+  final String label;
+  final String? hintText;
+  final TextEditingController? controller;
+  final bool isPassword;
+  final TextInputType keyboardType;
+  final String? Function(String?)? validator;
+  final IconData? prefixIcon;
+  final int? maxLines;
+
+  const FluentaTextField({
+    super.key,
+    required this.label,
+    this.hintText,
+    this.controller,
+    this.isPassword = false,
+    this.keyboardType = TextInputType.text,
+    this.validator,
+    this.prefixIcon,
+    this.maxLines = 1,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: AppTextStyles.labelMedium.copyWith(
+            color: AppColors.textPrimary,
+          ),
+        ),
+        const SizedBox(height: AppDimensions.s8),
+        TextFormField(
+          controller: controller,
+          obscureText: isPassword,
+          keyboardType: keyboardType,
+          validator: validator,
+          maxLines: maxLines,
+          decoration: InputDecoration(
+            hintText: hintText,
+            prefixIcon: prefixIcon != null ? Icon(prefixIcon, size: 20) : null,
+          ),
+        ),
+      ],
+    );
+  }
+}
