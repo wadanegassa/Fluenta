26aa43023339e4d0f0a634d4d56b83040930b395
wada negassa <promisehispeace@gmail.com>
2026-05-18 10:53:13 +0300

    test: update widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:fluenta/app.dart';
+import 'package:voce/app.dart';
 
 void main() {
   testWidgets('Smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const ProviderScope(child: FluentaApp()));
-    expect(find.text('Fluenta'), findsOneWidget);
+    await tester.pumpWidget(const ProviderScope(child: VoceApp()));
+    expect(find.text('Voce'), findsOneWidget);
   });
 }
